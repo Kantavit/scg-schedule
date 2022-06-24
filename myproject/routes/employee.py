e29ef333d72a808ee77d0f540c7a8e8678e9d08f
kantavit <krite-18354@hotmail.com>
2022-06-24 08:31:14 +0700

    show userId

--- myproject/routes/employee.py
+++ myproject/routes/employee.py
@@ -28,13 +28,14 @@ def employeePage():
         #     return render_template('employee/employee.html',userDetails=userDetails)
 
         line_userid = request.args.get("userId")
-        # cur = db.connection.cursor()
+        cur = db.connection.cursor()
         # query = "SELECT * FROM employee a , employeeInfo b WHERE a.line_id = " + line_userid + " AND a.employee_id = b.employee_id"
-        # user_name = cur.execute(query)
-        # userDetails = cur.fetchall()
-        # cur.close()
+        query = "SELECT * FROM employee WHERE line_id = " + line_userid
+        user_name = cur.execute(query)
+        userDetails = cur.fetchall()
+        cur.close()
 
-        return render_template('employee/employee.html', user_name=line_userid)
+        return render_template('employee/employee.html', user_name=userDetails)
         
 
 @employee.route('/employee/edit', methods=['POST','GET'])
