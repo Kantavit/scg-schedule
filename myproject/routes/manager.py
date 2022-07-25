00298a1900f8f7132f0c30aa1be588c9ef28352b
Kantavit <krite-18354@hotmail.com>
2022-07-25 23:46:31 +0700

    manager can submit coworkShift

--- myproject/routes/manager.py
+++ myproject/routes/manager.py
@@ -237,13 +237,224 @@ def editYourselfList():
 @manager.route('/manager/edit/cowork', methods=['POST','GET'])
 def editCowork():
     line_id = session.get("line_id") # in case for query
-        
+    sub_team = session.get("sub_team")
+    approver_id = session.get("approver_id")
+    
     if line_id is None or session.get("first_name") == "userNotFound":
-        return render_template('manager/warning.html')
+        return render_template('employee/warning.html')
+    
+    elif request.method == 'POST':
+        if request.form['choose'] == "สองคน":
+            cur = db.connection.cursor()
+
+            # count employee
+            cur.execute("SELECT COUNT(employee_id) FROM employeeInfo WHERE sub_team=%s",[sub_team])
+            employee_count = cur.fetchall()
+            count = employee_count[0][0]
+
+            # get employee in same team
+            cur.execute("SELECT employee_id, employee_name, employee_lastname FROM employeeInfo WHERE sub_team=%s",[sub_team])
+            idSub_team = cur.fetchall()
+            
+            employee_id1 = request.form['name2-1']
+
+            for i in range(count):
+                if idSub_team[i][0] == employee_id1:
+                    employee_name1 = idSub_team[i][1] 
+                    employee_lastname1 = idSub_team[i][2]
+                    break
+
+            date = request.form['date2-1']
+            OldShift = request.form['OldShift2-1']
+            NewShift = request.form['NewShift2-1']
+            employee_id2 = request.form['name2-2']
+            
+            for i in range(count):
+                if idSub_team[i][0] == employee_id2:
+                    employee_name2 = idSub_team[i][1] 
+                    employee_lastname2 = idSub_team[i][2]
+                    break
+                    
+            cur.close()
+            
+            OldShift2 = request.form['OldShift2-2']
+            NewShift2 = request.form['NewShift2-2']
+            reason = request.form['reason']
+            current_time = datetime.datetime.now()
+            TimeStamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+              
+            cur = db.connection.cursor()
+
+            session['date2-1'] = date
+
+            session['name2-1'] = employee_id1
+            session['employee_name1'] = employee_name1
+            session['employee_lastname1'] = employee_lastname1
+            session['OldShift2-1'] = OldShift
+            session['NewShift2-1'] = NewShift
+
+            session['name2-2'] = employee_id2
+            session['employee_name2'] = employee_name2
+            session['employee_lastname2'] = employee_lastname2
+            session['OldShift2-2'] = OldShift2
+            session['NewShift2-2'] = NewShift2
+
+            session['reason2p'] = reason
+            session['TimeStamp2p'] = TimeStamp
+            session['approver_id2p'] = approver_id
+            session['status2p'] = "approve"
+            session['choose'] = "สองคน"
+
+            cur.close()
+            return redirect(url_for('manager.employeeCoworkTransaction'))
+
+        elif request.form['choose'] == "สามคน":
+            cur = db.connection.cursor()
+
+            # count employee
+            cur.execute("SELECT COUNT(employee_id) FROM employeeInfo WHERE sub_team=%s",[sub_team])
+            employee_count = cur.fetchall()
+            count = employee_count[0][0]
+
+            # get employee in same team
+            cur.execute("SELECT employee_id, employee_name, employee_lastname FROM employeeInfo WHERE sub_team=%s",[sub_team])
+            idSub_team = cur.fetchall()
+
+            employee_id1 = request.form['name3-1']
+
+            for i in range(count):
+                if idSub_team[i][0] == employee_id1:
+                    employee_name1 = idSub_team[i][1] 
+                    employee_lastname1 = idSub_team[i][2]
+                    break
+
+            date = request.form['date3-1']
+            OldShift = request.form['OldShift3-1']
+            NewShift = request.form['NewShift3-1']
+            employee_id2 = request.form['name3-2']
+
+            for i in range(count):
+                if idSub_team[i][0] == employee_id2:
+                    employee_name2 = idSub_team[i][1] 
+                    employee_lastname2 = idSub_team[i][2]
+                    break
+                    
+            cur.close()
+
+            OldShift2 = request.form['OldShift3-2']
+            NewShift2 = request.form['NewShift3-2']
+            employee_id3 = request.form['name3-3']
+
+            for i in range(count):
+                if idSub_team[i][0] == employee_id3:
+                    employee_name3 = idSub_team[i][1] 
+                    employee_lastname3 = idSub_team[i][2]
+                    break
+                    
+            cur.close()
+
+            OldShift3 = request.form['OldShift3-3']
+            NewShift3 = request.form['NewShift3-3']
+            reason = request.form['reason']
+            current_time = datetime.datetime.now()
+            TimeStamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+              
+            cur = db.connection.cursor()
+
+            session['date3-1'] = date
+
+            session['name3-1'] = employee_id1
+            session['employee_name1'] = employee_name1
+            session['employee_lastname1'] = employee_lastname1
+            session['OldShift3-1'] = OldShift
+            session['NewShift3-1'] = NewShift
+
+            session['name3-2'] = employee_id2
+            session['employee_name2'] = employee_name2
+            session['employee_lastname2'] = employee_lastname2
+            session['OldShift3-2'] = OldShift2
+            session['NewShift3-2'] = NewShift2
+
+            session['name3-3'] = employee_id3
+            session['employee_name3'] = employee_name3
+            session['employee_lastname3'] = employee_lastname3
+            session['OldShift3-3'] = OldShift3
+            session['NewShift3-3'] = NewShift3
+
+            session['reason3p'] = reason
+            session['TimeStamp3p'] = TimeStamp
+            session['approver_id3p'] = approver_id
+            session['status3p'] = "approve"
+            session['choose'] = "สามคน"
+
+            cur.close()
+            return redirect(url_for('manager.employeeCoworkTransaction'))
+
     else:
-        return render_template('manager/coworkEdit.html', first_name=session.get("first_name"), last_name=session.get("last_name"))
-    # return render_template('manager/coworkEdit.html')
-    # ถ้าคนเป็น 2 ให้รับค่าแค่ 2 แถวแรก ถ้า 3 คนให้เอาทั้ง 3 แถว ถ้าเลือก 2 คนใส่มาคนเดียวให้ซ้ำหน้าเดิม
+        cur = db.connection.cursor()
+        
+        # count employee
+        cur.execute("SELECT COUNT(employee_id) FROM employeeInfo WHERE sub_team=%s",[sub_team])
+        employee_count = cur.fetchall()
+        count = employee_count[0][0]
+
+        # get employee in same team
+        cur.execute("SELECT employee_id, employee_name, employee_lastname FROM employeeInfo WHERE sub_team=%s",[sub_team])
+        idSub_team = cur.fetchall()
+
+        otherEmployee = [0]*count
+        for i in range(count):
+            otherEmployee[i] = idSub_team[i][0]
+        
+        if count == 1:
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[0]])
+            workData1 = cur.fetchall()
+            cur.close()
+            return render_template('manager/coworkEdit.html', first_name=session.get("first_name"), last_name=session.get("last_name"),
+                                idSub_team=idSub_team, workData1=workData1)
+        elif count == 2:
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[0]])
+            workData1 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[1]])
+            workData2 = cur.fetchall()
+            cur.close()
+            return render_template('manager/coworkEdit.html', first_name=session.get("first_name"), last_name=session.get("last_name"),
+                                idSub_team=idSub_team, workData1=workData1, workData2=workData2)
+        elif count == 3:
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[0]])
+            workData1 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[1]])
+            workData2 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[2]])
+            workData3 = cur.fetchall()
+            cur.close()
+            return render_template('manager/coworkEdit.html', first_name=session.get("first_name"), last_name=session.get("last_name"),
+                                idSub_team=idSub_team, workData1=workData1, workData2=workData2, workData3=workData3)
+        elif count == 4:
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[0]])
+            workData1 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[1]])
+            workData2 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[2]])
+            workData3 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[3]])
+            workData4 = cur.fetchall()
+            cur.close()
+            return render_template('manager/coworkEdit.html', first_name=session.get("first_name"), last_name=session.get("last_name"),
+                                idSub_team=idSub_team, workData1=workData1, workData2=workData2, workData3=workData3, workData4=workData4)
+        elif count == 5:
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[0]])
+            workData1 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[1]])
+            workData2 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[2]])
+            workData3 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[3]])
+            workData4 = cur.fetchall()
+            cur.execute("SELECT employee_id,Remark , dayoff FROM filtershift INNER JOIN employeeInfo ON filtershift.section_code = employeeInfo.section_code WHERE employee_id=%s",[otherEmployee[4]])
+            workData5 = cur.fetchall()
+            return render_template('manager/coworkEdit.html', first_name=session.get("first_name"), last_name=session.get("last_name"),
+                                idSub_team=idSub_team, workData1=workData1, workData2=workData2, workData3=workData3, workData4=workData4, workData5=workData5)
 
 
 @manager.route('/manager/edit/addshift', methods=['POST','GET'])
@@ -459,12 +670,97 @@ def employeeSelfTransaction():
 @manager.route('/manager/edit/cowork/coworksummary', methods=['POST','GET'])
 def employeeCoworkTransaction():
     line_id = session.get("line_id") # in case for query
-        
+    approver_id = session.get("approver_id")
+    requestId = approver_id
+
     if line_id is None or session.get("first_name") == "userNotFound":
-        return render_template('manager/warning.html')
+        return render_template('employee/warning.html')
+
+    elif session.get("choose") == "สองคน":
+        date1 = session.get("date2-1")
+
+        name1 = session.get("name2-1")
+        employee_name1 = session.get("employee_name1")
+        employee_lastname1 = session.get("employee_lastname1")
+        OldShift1 = session.get("OldShift2-1")
+        NewShift1 = session.get("NewShift2-1")
+
+        name2 = session.get("name2-2")
+        employee_name2 = session.get("employee_name2")
+        employee_lastname2 = session.get("employee_lastname2")
+        OldShift2 = session.get("OldShift2-2")
+        NewShift2 = session.get("NewShift2-2")
+
+        employee_name3 = None
+
+        reason = session.get("reason2p")
+        approver_id = session.get("approver_id2p")
+        status = "approve"
+        
+        if request.method == 'POST':
+            if request.form['choose'] == "cancel":
+                return redirect(url_for('manager.editCowork'))
+
+            elif request.form['choose'] == "confirm":
+                current_time = datetime.datetime.now()
+                TimeStamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+                cur = db.connection.cursor()
+                cur.execute("INSERT INTO transactionCoworkShift (requestId, employee_id , date , OldShift , NewShift , TimeStamp ,  reason , status , approver_id, employee_id2, employee_name2, employee_lastname2, OldShift2, NewShift2 ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(requestId, name1 , date1 , OldShift1 , NewShift1 , TimeStamp ,  reason , status , approver_id, name2, employee_name2, employee_lastname2, OldShift2, NewShift2))
+                db.connection.commit()
+                cur.close()
+                return redirect(url_for('manager.employeeCoworkTransactionEnd'))
+
+        return render_template('manager/coworkEditListSummary.html', first_name=session.get("first_name"), last_name=session.get("last_name"),
+                    date1=date1,name1=name1,employee_name1=employee_name1,employee_lastname1=employee_lastname1,OldShift1=OldShift1,NewShift1=NewShift1,
+                    employee_name2=employee_name2,employee_lastname2=employee_lastname2,OldShift2=OldShift2,NewShift2=NewShift2,reason=reason, employee_name3=employee_name3)
+
+    elif session.get("choose") == "สามคน":
+        date1 = session.get("date3-1")
+
+        name1 = session.get("name2-1")
+        employee_name1 = session.get("employee_name1")
+        employee_lastname1 = session.get("employee_lastname1")
+        OldShift1 = session.get("OldShift3-1")
+        NewShift1 = session.get("NewShift3-1")
+
+        name2 = session.get("name3-2")
+        employee_name2 = session.get("employee_name2")
+        employee_lastname2 = session.get("employee_lastname2")
+        OldShift2 = session.get("OldShift3-2")
+        NewShift2 = session.get("NewShift3-2")
+
+        name3 = session.get("name3-3")
+        employee_name3 = session.get("employee_name3")
+        employee_lastname3 = session.get("employee_lastname3")
+        OldShift3 = session.get("OldShift3-3")
+        NewShift3 = session.get("NewShift3-3")
+
+        reason = session.get("reason3p")
+        approver_id = session.get("approver_id3p")
+        status = "approve"
+        
+        if request.method == 'POST':
+            if request.form['choose'] == "cancel":
+                return redirect(url_for('manager.editCowork'))
+
+            elif request.form['choose'] == "confirm":
+                current_time = datetime.datetime.now()
+                TimeStamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+                cur = db.connection.cursor()
+                cur.execute("INSERT INTO transactionCoworkShift (requestId, employee_id , date , OldShift , NewShift , TimeStamp ,  reason , status , approver_id, employee_id2, employee_name2, employee_lastname2, OldShift2, NewShift2, employee_id3, employee_name3, employee_lastname3, OldShift3, NewShift3 ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(requestId, name1 , date1 , OldShift1 , NewShift1 , TimeStamp ,  reason , status , approver_id, name2, employee_name2, employee_lastname2, OldShift2, NewShift2, name3, employee_name3, employee_lastname3, OldShift3, NewShift3))
+                db.connection.commit()
+                cur.close()
+                return redirect(url_for('manager.employeeCoworkTransactionEnd'))
+
+        return render_template('manager/coworkEditListSummary.html', first_name=session.get("first_name"), last_name=session.get("last_name"),
+                    date1=date1,name1=name1,employee_name1=employee_name1,employee_lastname1=employee_lastname1,OldShift1=OldShift1,NewShift1=NewShift1,
+                    employee_name2=employee_name2,employee_lastname2=employee_lastname2,OldShift2=OldShift2,NewShift2=NewShift2,employee_name3=employee_name3,
+                    employee_lastname3=employee_lastname3,OldShift3=OldShift3,NewShift3=NewShift3,reason=reason)
+        
     else:
         return render_template('manager/coworkEditListSummary.html', first_name=session.get("first_name"), last_name=session.get("last_name"))
-    # return render_template('manager/coworkEditListSummary.html')
 
 
 @manager.route('/manager/edit/addshift/addshiftsummary', methods=['POST','GET'])
