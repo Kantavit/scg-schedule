55e838f2fdb70167d3d4c5b63cc57624eb077948
Kantavit <krite-18354@hotmail.com>
2022-08-03 13:33:31 +0700

    approver get emailed after selfTransactionend

--- myproject/extensions.py
+++ myproject/extensions.py
@@ -2,8 +2,10 @@ from flask_migrate import Migrate
 from flask_mysqldb import MySQL
 from flask import session
 from flask_session import Session
-
+import yagmail
+from .config import email, client_secret
 
 db = MySQL()
 migrate = Migrate()
 sess = Session()
+yag = yagmail.SMTP(email,oauth2_file=client_secret)
--- myproject/routes/employee.py
+++ myproject/routes/employee.py
@@ -1,5 +1,5 @@
 from .__init__ import employee
-from ..extensions import db
+from ..extensions import db, yag
 from flask import render_template, redirect, url_for, request, session
 import datetime;
 
@@ -1309,13 +1309,36 @@ def employeeAddTransaction():
 @employee.route('/employee/edit/shift/self/selflist/selflistsummary/selftransactionend', methods=['POST','GET'])
 def employeeSelfTransactionEnd():
     line_id = session.get("line_id") # in case for query
+    employee_id = session.get("employee_id")
         
     if line_id is None or session.get("first_name") == "userNotFound":
         return render_template('employee/warning.html')
     else:
+        cur = db.connection.cursor()
+        cur.execute("SELECT approver_name,approver_lastname,approver_email FROM `approverInfo` INNER JOIN employeeInfo ON approverInfo.approver_id = employeeInfo.approver_id WHERE employeeInfo.employee_id=%s",[employee_id])
+        approver_data = cur.fetchall()
+        approver_name = approver_data[0][0]
+        approver_lastname = approver_data[0][1]
+        approver_email = approver_data[0][2]
+
+        cur.execute("SELECT employee_name, employee_lastname FROM employeeInfo WHERE employee_id=%s",[employee_id])
+        employee_data = cur.fetchall()
+        employee_name = employee_data[0][0]
+        employee_lastname = employee_data[0][1]
+        cur.close()
+
+        current_time = datetime.datetime.now()
+        TimeStamp = current_time.strftime("%Y-%m-%d")
+
+        recipients = [approver_email]
+        subject = 'Test ตารางกะ'
+        body = f'เรียน {approver_name} {approver_lastname},\n\nอีเมล์นี้เป็นอีเมล์อัตโนมัติทีส่งจากระบบ SCG-Schedule\n\nด้วยความเคารพ,\nโปรดตรวจสอบการขออนุมัติ (จากคุณ {employee_name} {employee_lastname} เมื่อวันที่ {TimeStamp} กรุณาพิจารณารายการผ่านทางลิงก์ด้านล่าง http://127.0.0.1:5000/manager'
+        yag.useralias = 'testbyNamhvam'
+        yag.send(to=recipients,subject=subject,contents=[body])
+        print ('ส่ง Email สำเร็จ')
+        
         return render_template('employee/selfTransactionEnd.html', first_name=session.get("first_name"), last_name=session.get("last_name"))
-    # return render_template('employee/selfTransactionEnd.html')
-    # บันทึกลง table transaction
+
 
 @employee.route('/employee/edit/shift/cowork/coworksummary/coworktransactionend', methods=['POST','GET'])
 def employeeCoworkTransactionEnd():
